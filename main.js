64566c26832611a4781ad91cccb5eed931d980b7
spinheiro93 <sampin@bu.edu>
2016-07-06 19:43:01 +0100

    session 4 half way

---
+++ main.js
@@ -0,0 +1,22 @@
+$( window ).load(function(){
+	$( "#fruit-list" ).click(function() {
+	  alert( "Dont eat me." );
+	});
+
+	$('h1').hide()
+
+	$('h1').show()
+
+	$('h1').css('color', '#f09')
+
+	  $('h1').css('font-size', '80px')
+
+	$('h1').css('text-align', 'right')*
+	  
+	  $('#fruit-list').css('color', 'pink')
+
+	$('h1').fadeOut()
+
+	$('h1').fadeIn()
+
+});
